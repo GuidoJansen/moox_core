c346ae63b33ee168aa8aa3feecdff8083b1e5cfa
Jakub Czyz <info@jakub-czyz.de>
2014-09-05 10:52:01 +0200

    Incorrect path to backend.css

--- Classes/Hooks/Backend/PreHeaderRender.php
+++ Classes/Hooks/Backend/PreHeaderRender.php
@@ -12,7 +12,7 @@ class PreHeaderRender {
      */
     public function addStyles(&$params, &$documentTemplate){
         $backendCssFile = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('moox_core') . 'Resources/Public/Stylesheets/backend.css';
-        $params['pageRenderer']->addCssFile('moox_core',$backendCssFile);
+        $params['pageRenderer']->addCssFile($backendCssFile,'stylesheet');
     }
 
 }
