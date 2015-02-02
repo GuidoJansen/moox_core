9547b11951e0f9187129472782e9632b2582bc75
Jakub Czyz <info@jakub-czyz.de>
2015-02-02 13:39:12 +0100

    Use new Flux recursive array utility

--- Classes/Controller/AbstractCoreContentController.php
+++ Classes/Controller/AbstractCoreContentController.php
@@ -24,8 +24,7 @@ namespace FluidTYPO3\MooxCore\Controller;
  *  This copyright notice MUST APPEAR in all copies of the script!
  *****************************************************************/
 use FluidTYPO3\Flux\Controller\AbstractFluxController;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Domain\Repository\CategoryRepository;
+use FluidTYPO3\Flux\Utility\RecursiveArrayUtility;
 /**
  * Class AbstractCoreContentController
  */
@@ -50,7 +49,7 @@ abstract class AbstractCoreContentController extends AbstractFluxController {
 	protected function initializeViewVariables() {
 		$row = $this->getRecord();
 		$flexFormData = $this->configurationService->convertFlexFormContentToArray($row['pi_flexform']);
-		$this->settings = GeneralUtility::array_merge_recursive_overrule($this->settings, $flexFormData, FALSE, FALSE);
+		$this->settings = RecursiveArrayUtility::merge($this->settings, $flexFormData, FALSE, FALSE);
 		parent::initializeViewVariables();
 	}
 }
