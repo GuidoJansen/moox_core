e1c6974ced51e924d95d4a69c53db1dd9f0ac64b
GJansen <gjansen@coco-new-media.de>
2016-04-08 14:53:19 +0000

    [TASK] Changes for PULS Powersupplies

--- Configuration/RealURL/Default.php
+++ Configuration/RealURL/Default.php
@@ -1,5 +1,21 @@
 <?php
+// function user_encodeSpURL_postProc(&$params, &$ref) {
+    // $params['URL'] = str_replace('article/datum', 'article', $params['URL']);
+	// $params['URL'] = str_replace('news/news', 'news', $params['URL']);
+// }
+// function user_decodeSpURL_preProc(&$params, &$ref) {
+    // $params['URL'] = str_replace('article', 'article/datum', $params['URL']);
+	
+	
+// }
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl'] = array(
+    'encodeSpURL_postProc' => array('user_encodeSpURL_postProc'),
+    'decodeSpURL_preProc' => array('user_decodeSpURL_preProc'));
+
+
 $TYPO3_CONF_VARS['FE']['addRootLineFields'].= ',tx_realurl_pathsegment';
+
 $TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT'] = array(
         'pagePath' => array(
                 'type' => 'user',
@@ -7,7 +23,8 @@ $TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT'] = array(
                 'spaceCharacter' => '-',
                 'languageGetVar' => 'L',
                 'expireDays' => '3',
-                'firstHitPathCache'=>1
+                'firstHitPathCache'=>1,
+				'rootpage_id' => '36'
         ),
         'init' => array(
                 'enableCHashCache' => TRUE,
@@ -21,7 +38,7 @@ $TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT'] = array(
                 array(
                         'GETvar' => 'L',
                         'valueMap' => array(
-                                'en' => '1',
+                                'de' => '1',
                         ),
                         'noMatch' => 'bypass',
                 ),
@@ -47,85 +64,29 @@ $TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT'] = array(
                     ),
                 ),
             ),
-            'defaultToHTMLsuffixOnPrev' => '.html',
+            'defaultToHTMLsuffixOnPrev' => '',
         ),
         'postVarSets' => array(
                 '_DEFAULT' => array(
-                        'controller' => array(
-                                array(
-                                        'GETvar' => 'tx_mooxnews_pi1[action]',
-                                        'noMatch' => 'bypass'
-                                ),
-                                array(
-                                        'GETvar' => 'tx_mooxnews_pi1[controller]',
-                                        'noMatch' => 'bypass'
-                                )
-                        ),
-                        'dateFilter' => array(
-                                array(
-                                        'GETvar' => 'tx_mooxnews_pi1[overwriteDemand][year]',
-                                ),
-                                array(
-                                        'GETvar' => 'tx_mooxnews_pi1[overwriteDemand][month]',
-                                ),
-                        ),
-                        'archive' => array (
-                                '0' => array (
-                                        'GETvar' => 'tx_mooxnews_pi1[year]',
-                                ),
-                                '1' => array (
-                                        'GETvar' => 'tx_mooxnews_pi1[month]',
-                                        'valueMap' => array (
-                                                'january' => '01',
-                                                'february' => '02',
-                                                'march' => '03',
-                                                'april' => '04',
-                                                'may' => '05',
-                                                'june' => '06',
-                                                'july' => '07',
-                                                'august' => '08',
-                                                'september' => '09',
-                                                'october' => '10',
-                                                'november' => '11',
-                                                'december' => '12',
-                                        ),
-                                ),
-                        ),
-                        'browse' => array (
-                                '0' => array (
-                                        'GETvar' => 'tx_mooxnews_pi1[pointer]',
-                                ),
-                        ),
-                        'select_category' => array (
-                                '0' => array (
-                                        'GETvar' => 'tx_mooxnews_pi1[cat]',
-                                ),
-                        ),
-                        'article' => array (
-                                '0' => array (
-                                        'GETvar' => 'tx_mooxnews_pi1[news]',
-                                        'lookUpTable' => array (
-                                                'table' => 'tx_mooxnews_domain_model_news',
-                                                'id_field' => 'uid',
-                                                'alias_field' => 'title',
-                                                'addWhereClause' => ' AND NOT deleted',
-                                                'useUniqueCache' => '1',
-                                                'useUniqueCache_conf' => array (
-                                                    'strtolower' => '1',
-                                                    'spaceCharacter' => '-',
-                                                ),
-                                        ),
-                                ),
-                                '1' => array (
-                                        'GETvar' => 'tx_mooxnews_pi1[swords]',
-                                ),
-                        ),
-                        'page' => array(
-                                array(
-                                        'GETvar' => 'tx_mooxnews_pi1[@widget_0][currentPage]',
-                                ),
-                        ),
-                ),
+                 
+									 'browse' => array( 
+                                                       array( 'GETvar' => 'tx_ttnews[pointer]', 'valueMap' => array('weiter' => '1','weiter' => '2',)),),
+ 
+ 
+                                      // news kategorien
+ 
+                                      'kategorie' => array ( 
+                                                      array( 'GETvar' => 'tx_ttnews[cat]', 'lookUpTable' => array('table' => 'tt_news_cat', 'id_field' => 'uid', 'alias_field' => 'title', 'addWhereClause' => ' AND NOT deleted', 'useUniqueCache' => 1, 'useUniqueCache_conf' => array( 'strtolower' => 1, 'spaceCharacter' => '-',    ),),),),
+                                           
+ 
+                                      // news artikel
+ 
+                                      'datum' => array(
+                                                                           
+                                              
+                                              
+                                              array('GETvar' => 'tx_ttnews[tt_news]','lookUpTable' => array( 'table' => 'tt_news', 'id_field' => 'uid', 'alias_field' => 'title', 'addWhereClause' => ' AND NOT deleted', 'useUniqueCache' => 1, 'useUniqueCache_conf' => array( 'strtolower' => 1, 'spaceCharacter' => '-',  ),),),),	
+									  ),
         ),
 );
 
